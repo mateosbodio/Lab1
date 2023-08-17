154d9cb018e6af06993d19d25413a80499eba663
mateosbodio <141843461+mateosbodio@users.noreply.github.com>
2023-08-17 16:38:55 -0300

    Ejemplo1

--- src/tp3_lab1/tp3_lab1.java
+++ src/tp3_lab1/tp3_lab1.java
@@ -24,7 +24,7 @@ public class tp3_lab1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("GOOOLLLL");
     }
     
 }
