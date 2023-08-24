fe29fc28b23a22e2e0b0d2b5c39ee1db911d506f
mateosbodio <141843461+mateosbodio@users.noreply.github.com>
2023-08-24 16:56:36 -0300

    tp4_listo

---
+++ src/tp4_lab1/tp4_lab1.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp4_lab1;
+
+/**
+ *
+ * @author Mateo
+ */
+/*
+Ejercicio 2:
+
+Crea una aplicación GUI en Java que simule un convertidor de unidades de temperatura. La interfaz debe contener los siguientes elementos:
+
+Un campo de texto (JTextField) para que el usuario pueda ingresar la temperatura en grados Celsius.
+Un botón (JButton) con el texto "Convertir".
+Cuando el usuario ingrese la temperatura en grados Celsius y haga clic en el botón "Convertir", la aplicación debe realizar la conversión de Celsius a Fahrenheit y mostrar el resultado en un cuadro de diálogo emergente.
+
+La fórmula para convertir de grados Celsius a Fahrenheit es: Fahrenheit = Celsius * 9/5 + 32.
+
+Recuerda utilizar la biblioteca de componentes gráficos de Java (Swing) para crear la interfaz y gestionar el evento del botón.
+
+Subir proyecto a GitHub y enviar link al repositorio.
+*/
+public class tp4_lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      JFrameConv temp= new JFrameConv();
+      temp.setVisible(true);
+        
+                
+    }
+    
+}
