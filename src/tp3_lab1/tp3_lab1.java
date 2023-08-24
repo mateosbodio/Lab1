45c187c74cd41e9cdc7a785c0d5902f9392f5137
mateosbodio <141843461+mateosbodio@users.noreply.github.com>
2023-08-24 11:42:30 -0300

    tp3

--- src/tp3_lab1/tp3_lab1.java
+++ src/tp3_lab1/tp3_lab1.java
@@ -1,1 +1,34 @@
-//cambio
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_lab1;
+
+/**
+ *
+ * @author Mateo
+ */
+/*
+Ejercicio 1:
+
+Crea una aplicación GUI en Java que simule un formulario de login. La interfaz debe contener los siguientes elementos:
+
+Un campo de texto (JTextField) para que el usuario pueda ingresar su mail.
+Un campo de texto (JTextField) para que el usuario pueda ingresar su password.
+Un botón (JButton) con el texto "Registrar".
+Cuando el usuario haga clic en el botón "Registrar", la aplicación debe mostrar un diálogo emergente (JOptionPane) con un mensaje de bienvenida si el mail es igual a alumno@ulp.edu.ar y el password es igual a “12345678”, caso contrario el mensaje debe decir “Usuario y/o contraseña incorrectos” .
+
+Recuerda que debes utilizar la biblioteca de componentes gráficos de Java (Swing) para crear la interfaz y gestionar los eventos del botón.
+
+Sube el Proyecto a GitHub y envía el link a través de la tarea.
+*/
+public class tp3_lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    }
+    
+}
