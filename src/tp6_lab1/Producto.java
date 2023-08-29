469d648dc57499572cdd411643e135844912ef58
mateosbodio <141843461+mateosbodio@users.noreply.github.com>
2023-08-29 13:52:11 -0300

    tp5 listo

---
+++ src/tp6_lab1/Producto.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp6_lab1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Producto implements Comparable<Producto> {
+    private int codigo;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private String rubro;
+
+    public Producto(int codigo, String descripcion, double precio, int stock, String rubro) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.rubro = rubro;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(String rubro) {
+        this.rubro = rubro;
+    }
+
+    @Override
+    public int compareTo(Producto t) {
+        
+        if(this.codigo==t.codigo){
+        
+            return 0;
+        }else if(this.codigo<t.codigo){
+        
+            return -1;
+        } else{
+        
+            return 1;
+        }
+        
+    }
+
+    
+    
+    
+}
